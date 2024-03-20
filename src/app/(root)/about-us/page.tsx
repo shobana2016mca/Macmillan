403de1956e77963e67d8031d5864e309725accca
shobana007 <shobana.r@gravitonweb.com>
2024-03-20 23:52:09 +0530

    about

--- src/app/(root)/about-us/page.tsx
+++ src/app/(root)/about-us/page.tsx
@@ -3,10 +3,102 @@ import Image from "next/image";
 export default function AboutUs() {
   return (
     <div>
-      AboutUs
-      <div className="relative h-80 w-full">
-        <Image src={"/hero-bg.jpeg"} alt="img" fill />
-      </div>
+     
+      {/* <div className="relative h-80 w-full bg-purple-900 opacity-80 blur-sm">
+        <Image src={"/aboutbg.jpg"} alt="img" fill />
+      </div> */}
+      
+      <section className="relative pt-[112px] min-h-[50vh] overflow-y-hidden pb-[90px] bg-[rgba(78,0,122,0.6)] xl:px-40">
+        <div className="z-20 grid grid-cols-1 md:grid-cols-1 relative place-items-center">
+          
+          <div className="text-white text-center">
+            <div className="pr-30">
+              <h1 className="text-5xl font-bold">
+                About us
+              </h1>
+
+             
+            </div>
+
+           
+          </div>
+        </div>
+
+        <div className="bg-inherit absolute left-0 top-0 z-[1]"></div>
+        <div
+          className="h-[200%] absolute inset-0 bg-fixed z-[-1] bg-cover"
+          style={{
+            backgroundImage:
+              "url('/aboutbg.jpg')",
+            backgroundPosition: "50% 0",
+          }}
+        ></div>
+      </section>
+
+   
+ 
+{/* <div className="grid grid-rows-3 grid-flow-col-2 gap-4">
+  <div className="row-start-2 row-span-2">01</div>
+  <div className="row-end-3 row-span-2">02</div>
+  
+</div> */}
+<div className="flex flex-wrap items-center p-8">
+  
+  <div className="w-full md:w-1/2 p-4">
+    <h2 className="text-xl md:text-3xl lg:text-3xl font-bold mb-4">About Macmillan HR</h2>
+    <h2 className="text-xl md:text-3xl lg:text-3xl font-bold mb-4">Helping people all around the world bag their dream job.
+    </h2>
+    <p className="text-base md:text-sm lg:text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+    <p className="text-base md:text-sm lg:text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+  </div>
+  <div className="w-full md:w-1/2 p-4">
+  <Image src={"/MapsAboutUs.jpg"}  height={800} width={800} alt="img"/>
+  </div>
+</div>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
+  <div className="flex flex-col sm:flex-row">
+  <div className="bg-gray-200 p-4 w-20">Column 1</div>
+  <div className="bg-gray-200 p-4 w-80">Column 2 Column 2 Column 2 </div>
+</div>
+<div className="flex flex-col sm:flex-row">
+  <div className="bg-gray-200 p-4 w-30">Column 1</div>
+  <div className="bg-gray-200 p-4 w-70">Column 2 Column 2 Column 2</div>
+</div>
+<div className="flex flex-col sm:flex-row">
+  <div className="bg-gray-200 p-4 w-30">Column 1</div>
+  <div className="bg-gray-200 p-4 w-70">Column 2 Column 2 Column 2</div>
+</div>  
+</div>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
+  <div className="bg-gray-200 p-4"><div className="flex items-center bg-gray-200 rounded-lg p-4">
+  <div className="mr-4">
+  <Image src={"/MapsAboutUs.jpg"}  height={80} width={80} alt="img"/>
+  </div>
+  <div>
+    <p className="text-lg font-semibold">Your Text Here</p>
+    <p className="text-sm text-gray-500">Additional Description</p>
+  </div>
+</div></div>
+  <div className="bg-gray-200 p-4">Column 2</div>
+  <div className="bg-gray-200 p-4">Column 3</div>
+  
+  
+</div>
+
+<div className="flex items-center bg-gray-200 rounded-lg p-4">
+  <div className="mr-4">
+  <Image src={"/MapsAboutUs.jpg"}  height={80} width={80} alt="img"/>
+  </div>
+  <div>
+    <p className="text-lg font-semibold">Your Text Here</p>
+    <p className="text-sm text-gray-500">Additional Description</p>
+  </div>
+</div>
+
+
     </div>
+
+  
+    
   );
 }
--- src/app/(root)/contact-us/page.tsx
+++ src/app/(root)/contact-us/page.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
 export default function ContactUs() {
-  return <div>ContactUs</div>;
+  return (
+    <div>
+     
+      {/* <div className="relative h-80 w-full bg-purple-900 opacity-80 blur-sm">
+        <Image src={"/aboutbg.jpg"} alt="img" fill />
+      </div> */}
+      
+      <div className="flex flex-col relative items-center justify-center h-80 overflow-hidden bg-gradient-to-r from-violet-500 to-fuchsia-500">
+  
+  <div className="h-full">
+  map
+  </div>
+</div>
+
+<div className="grid grid-rows-2 grid-flow-col gap-4">
+  <div className="row-span-2 col-span-2 p-20 w-3/4">Contact us</div>
+  <div className="row-span-2 col-span-2 p-20 w-1/4">01</div>
+ 
+</div>
+
+
+    </div>
+  );
 }
